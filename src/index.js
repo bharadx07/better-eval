494a7021e4fd2eed02f055427d57d46e7b64ad37
Bharadwaj Duggaraju <bharadwaj.duggaraju@outlook.com>
2022-01-06 22:24:45 -0800

    feat: flesh out the first version of the package

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
-"use strict"
+"use strict";
 
-function betterEval() {
-    console.log("Better Eval ... Comming Soon!")
+/**
+ * @description takes code to execute and the context to exec it in, and exec's the code in the context!
+ * @param {string} code
+ * @param {'local' | 'vm'} context
+ * @returns {any} if your evaluated code returns a value, then betterEval will return it to you.
+ */
+function betterEval(code, context) {
+  if (context === "local") {
+    /* option 1. we want to run the code on a local fn context */
+
+    // literal text of function to flush the value out
+    const flush = `return ${code}`
+
+    //construct function that runs the 'flush'
+    const flushFn = new Function(flush)
+
+    //run the function and return the value if exists
+    return flushFn()
+    
+  } else if (context === "vm") {
+    /* option 2/. we want to run the code on a local fn context */
+  } else {
+    /* otherwise: the context isn't right, throw an error */
+    throw new Error(
+      "Unsupported context for execution. Please choose between 'local' and 'vm'."
+    );
+  }
+
+  return 1;
 }
 
-module.exports = betterEval
+module.exports = betterEval;
