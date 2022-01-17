c768e071e5602e64a52ea934bab717c7ec99d761
Bharadwaj Duggaraju <bharadwaj.duggaraju@outlook.com>
2022-01-16 21:27:08 -0800

    feat: test - cant pass in function constructor

--- test/variables.test.js
+++ test/variables.test.js
@@ -42,3 +42,13 @@ test("should not be able to pass eval", () => {
   // test
   expect(failEval).toBe(expected);
 });
+
+test("should not be able to pass function constructor", () => {
+    // no pass eval
+    const failEval = betterEval("Function", { Function });
+    // expected
+    const expected = null;
+    // test
+    expect(failEval).toBe(expected);
+  });
+  
