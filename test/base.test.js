e43f6e740b814e0d4103adbf63ba4ce88e48c646
Bharadwaj Duggaraju <bharadwaj.duggaraju@outlook.com>
2022-01-16 20:47:05 -0800

    feat: multiple variables

--- test/base.test.js
+++ test/base.test.js
@@ -1,49 +1,46 @@
 const betterEval = require("../src");
 
-test('should evaluate a simple expression', () => {
-    // sum
-    const sum = betterEval("1+1");
-    //expected
-    const expected = 2;
-    //test
-    expect(sum).toBe(expected)
-})
+test("should evaluate a simple expression", () => {
+  // sum
+  const sum = betterEval("1+1");
+  //expected
+  const expected = 2;
+  //test
+  expect(sum).toBe(expected);
+});
 
-test('should join two strings', () => {
-    // joined
-    const joined = betterEval("'Hello ' + 'World'")
-    // expected
-    const expected = "Hello World"
-    // test
-    expect(joined).toBe(expected)
-})
+test("should join two strings", () => {
+  // joined
+  const joined = betterEval("'Hello ' + 'World'");
+  // expected
+  const expected = "Hello World";
+  // test
+  expect(joined).toBe(expected);
+});
 
-test('should be able to call object functions', () => {
-    // uppercased
-    const uppercased = betterEval("'lowercase'.toUpperCase()");
-    // expected
-    const expected = "LOWERCASE"
-    // test
-    expect(uppercased).toBe(expected)
-})
+test("should be able to call object functions", () => {
+  // uppercased
+  const uppercased = betterEval("'lowercase'.toUpperCase()");
+  // expected
+  const expected = "LOWERCASE";
+  // test
+  expect(uppercased).toBe(expected);
+});
 
-test('should be able to get values from functions', () => {
-    // insideFn
-    const insideFn = betterEval("(() => 'hello')()")
-    // expected
-    const expected = "hello"
-    // test
-    expect(insideFn).toBe(expected)
-})
+test("should be able to get values from functions", () => {
+  // insideFn
+  const insideFn = betterEval("(() => 'hello')()");
+  // expected
+  const expected = "hello";
+  // test
+  expect(insideFn).toBe(expected);
+});
 
-
-test('should be able to access non destructive properties', () => {
-    // non destructive
-    const nonDestructive = betterEval("Array(5)");
-    // expected
-    const expected = Array(5)
-    // test
-    expect(nonDestructive).toEqual(expected)
-})
-
-
+test("should be able to access non destructive properties", () => {
+  // non destructive
+  const nonDestructive = betterEval("Array(5)");
+  // expected
+  const expected = Array(5);
+  // test
+  expect(nonDestructive).toEqual(expected);
+});
--- test/variables.test.js
+++ test/variables.test.js
@@ -20,3 +20,16 @@ test("should be able to call user made functions", () => {
   expect(greeting).toBe(expected);
 });
 
+test("should be able to pass in multiple variables", () => {
+  // multiple variables
+  const name1 = "Tom";
+  const getName2 = () => "Kate";
+  const greeting = betterEval("`Hey ${name1} and ${getName2()}`", {
+    name1,
+    getName2,
+  });
+  // expected
+  const expected = "Hey Tom and Kate";
+  // test
+  expect(greeting).toBe(expected);
+});
